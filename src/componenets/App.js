d2cca6c529daf33a5e77a8e075848b3b5dc773a5
Marina Almeida <67846587+MarinaAlmeida20@users.noreply.github.com>
2022-05-21 12:18:19 +0100

    JsonFile component created

--- src/componenets/App.js
+++ src/componenets/App.js
@@ -1,63 +1,13 @@
 import React from "react";
+import JsonFile from "./JsonFile"
 
 function App() {
   return <>
     <h1>Marina</h1>
     <JsonFile />
-  </>;
+  </>
 }
 
-function JsonFile() { 
-  let data = [{
-    "id": 5,
-    "moduleName": "DevOps",
-    "mainProject": {
-        "name": "DevOps",
-        "url": "https://github.com/CodeYourFuture/JavaScript-Core-3-Classwork-Week4-London8/"
-    },
-    "assignments": [
-        {
-            "name": "Run unit tests on Github",
-            "url": "https://github.com/CodeYourFuture/JavaScript-Core-3-Classwork-Week4-London8/"
-        },
-        {
-            "name": "DevOps Basic I",
-            "url": ""
-        },
-        {
-            "name": "DevOps Basic III",
-            "url": "https://github.com/CodeYourFuture/DevOps-Basics-Lesson-3-London8"
-        },
-        {
-            "name": "DevOps Advanced I - Docker",
-            "url": ""
-        },
-        {
-            "name": "DevOps Advanced II - More Docker",
-            "url": ""
-        }
-    ]
-}]
 
-
-  return (
-    <div>
-      {data.map(element => {
-        return ( 
-          <div>
-          <p>Module Name: {element.moduleName}</p>
-          <p>URL: <a href="element.mainProject.url">Github link</a></p>
-          <ul>
-            {element.assignments.map(el => {
-              return <li><a href={el.url}>{el.name}</a></li>
-            })}
-          </ul>
-          </div>
-        )
-      })}
-    </div>
-  )
-
- }
 
 export default App;
